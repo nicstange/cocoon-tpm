f0ac2bd94d14e23271c55118c5d05c7942a737c9
Nicolai Stange <nstange@suse.de>
2025-04-15 17:53:01 +0000

    crypto/ecc: implement ECC arithmetic for Weierstrass curves
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- crypto/src/ecc/mod.rs
+++ crypto/src/ecc/mod.rs
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2023-2025 SUSE LLC
+// Copyright 2023 SUSE LLC
 // Author: Nicolai Stange <nstange@suse.de>
 
-//! ECC implementation.
-
+pub mod curve;
