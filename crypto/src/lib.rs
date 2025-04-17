823d30e2623919d20cfffa79d694196217774488
Nicolai Stange <nstange@suse.de>
2025-04-17 13:54:58 +0000

    crypto: add empty mod pure_rust
    Introduce the empty mod pure_rust to move the pure Rust crypto primitive
    implementations into as BoringSSL based alternatives are getting successively
    introduced.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -17,8 +17,15 @@ pub mod rng;
 #[cfg(feature = "rsa")]
 pub mod rsa;
 pub mod symcipher;
+
+#[cfg(not(feature = "boringssl"))]
+mod pure_rust;
+#[cfg(not(feature = "boringssl"))]
+use pure_rust as backend;
 #[cfg(feature = "boringssl")]
 mod bssl_ffi;
+#[cfg(feature = "boringssl")]
+use bssl_ffi as backend;
 
 pub use error::*;
 pub use io_slices::*;
